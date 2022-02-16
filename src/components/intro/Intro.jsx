61c410c9a9ed1681bfcc445802bf196555cb7bb9
farhanbadr <farhan.badr@nebensolutions.ca>
2022-02-16 19:24:37 +0500

    Initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,44 @@
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Freelancer",
+        "Web Developer",
+        "Web Designer",
+        "Blockchain Developer",
+        "Mobile App Developer",
+      ],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/profile.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Awais Khan</h1>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
